be73c75c44e453932a936020f0d2769741395a6b
mmm <mxj1323610522@163.com>
2017-10-30 09:14:46 +0800

    1212

---
+++ public/js/index9b37934a2330d6037be7.js
@@ -0,0 +1,91 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+throw new Error("Cannot find module \"jquery\"");
+throw new Error("Cannot find module \"bootstrap/dist/css/bootstrap.css\"");
+throw new Error("Cannot find module \"bootstrap/dist/js/bootstrap.js\"");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__css_index_css__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__css_index_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3__css_index_css__);
+
+// import '../css/normalize.css';
+
+
+
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleNotFoundError: Module not found: Error: Can't resolve '../img/ban111111111111ner.jpg' in 'D:\\1\\1\\实训 张鹏\\作业\\one\\src\\css'\n    at factoryCallback (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\webpack\\lib\\Compilation.js:276:40)\n    at factory (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\webpack\\lib\\NormalModuleFactory.js:235:20)\n    at resolver (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\webpack\\lib\\NormalModuleFactory.js:60:20)\n    at asyncLib.parallel (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\webpack\\lib\\NormalModuleFactory.js:127:20)\n    at D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\async\\dist\\async.js:3861:9\n    at D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\async\\dist\\async.js:421:16\n    at iteratorCallback (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\async\\dist\\async.js:996:13)\n    at D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\async\\dist\\async.js:906:16\n    at D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\async\\dist\\async.js:3858:13\n    at resolvers.normal.resolve (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\webpack\\lib\\NormalModuleFactory.js:119:22)\n    at onError (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\Resolver.js:65:10)\n    at loggingCallbackWrapper (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at runAfter (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\Resolver.js:158:4)\n    at innerCallback (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\Resolver.js:146:3)\n    at loggingCallbackWrapper (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at next (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\tapable\\lib\\Tapable.js:252:11)\n    at D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\UnsafeCachePlugin.js:40:4\n    at loggingCallbackWrapper (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at runAfter (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\Resolver.js:158:4)\n    at innerCallback (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\Resolver.js:146:3)\n    at loggingCallbackWrapper (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at next (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\tapable\\lib\\Tapable.js:252:11)\n    at innerCallback (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\Resolver.js:144:11)\n    at loggingCallbackWrapper (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at next (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\tapable\\lib\\Tapable.js:249:35)\n    at resolver.doResolve.createInnerCallback (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\DescriptionFilePlugin.js:44:6)\n    at loggingCallbackWrapper (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at afterInnerCallback (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\Resolver.js:168:10)\n    at loggingCallbackWrapper (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\enhanced-resolve\\lib\\createInnerCallback.js:31:19)\n    at next (D:\\1\\1\\实训 张鹏\\作业\\one\\node_modules\\tapable\\lib\\Tapable.js:252:11)");
+
+/***/ })
+/******/ ]);
---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+import '../css/index.css'
+
+import 'Swiper/dist/css/swiper.css';
+import Swiper from 'Swiper/dist/js/swiper';
+var mySwiper = new Swiper('.swiper-container');
+    // mySwiper.enableTouchControl();//默认是开启的      
+
